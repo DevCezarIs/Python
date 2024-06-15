5e23aba2c9b29f22aba3b0e59f1fbf8a98ffbe57
DevCezarIs <cezar.dev24@gmail.com>
2024-06-14 23:02:37 -0300

    Created a fist past and file abbout the dict.

---
+++ Dicionario/Examples.py
@@ -0,0 +1,9 @@
+''' 
+
+Para realizar um dicionário precisamos de 2 elementos, a chave 
+ e o valor como no exemplo abaixo:
+
+'''
+
+pesssoa = {"gmail" : "cezar@gmail.com" , "nome" : "Cezar", "idade: " : 18}
+print(pesssoa["nome"])
