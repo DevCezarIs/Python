3255a3ea732ea1e084ce97fa1ed6ae8bf7e1228c
DevCezarIs <167385308+DevCezarIs@users.noreply.github.com>
2024-07-01 08:37:35 -0300

    so arrumei o inner_fuction

--- Python_POO/Decoradores/inner_fuction.py
+++ Python_POO/Decoradores/inner_fuction.py
@@ -1,17 +1,29 @@
 def calculadora(operação):
-    
+      
     def somar(a,b):
-        return a+b
-     
+          return a+b
+      
     def sub(a,b):
-        return a-b
+          return a-b
+      
+    def div(a,b):
+          return a/b
+      
+    def mult(a,b):
+          return a*b
 
     match operação:
         case "+":
             return(somar)
-        
+          
         case "-":
             return(sub)
+          
+        case "*":
+            return(mult)
+          
+        case "/":
+            return(div)
 
-hehe = calculadora("+")(5 , 6)
-print(hehe)
+a = calculadora("+")(5, 6)
+print(a)       
